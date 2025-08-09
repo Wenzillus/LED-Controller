bf91331b5a09b4a71a35019cd097c45534b2bd5c
[Niko] <[wenzel.aiman@gmail.com]>
2025-08-09 09:52:47 +0200

    Zwischenstand - Funktionabel

--- LED Controller/Services/AmbilightUdpService.cs
+++ LED Controller/Services/AmbilightUdpService.cs
@@ -46,23 +46,23 @@ namespace LED_Controller.Services
             client.Send(data, data.Length, controller.IpAddress, controller.Port);
         }
         //// Sendet eine komplette LED-Farbliste an den Controller
-        //public static void SendFrame(LedController controller, LedStrip strip, IList<Color> ledColors)
-        //{
-        //    using var client = new UdpClient();
-        //    int bytesPerLed = strip.Type == LedType.RGBW ? 4 : 3;
-        //    //var data = new byte[ledColors.Count * bytesPerLed + 1];
+        public static void SendFrame(LedController controller, LedStrip strip, IList<Color> ledColors)
+        {
+            using var client = new UdpClient();
+            int bytesPerLed = strip.Type == LedType.RGBW ? 4 : 3;
+            //var data = new byte[ledColors.Count * bytesPerLed + 1];
 
-        //    var data = new byte[136 * 4 + 60 * 3 + 23 * 3 + 3];
-        //    data[0] = 3;
-        //    for (int i = 0; i < ledColors.Count ; i++)
-        //    {
-        //        data[i * bytesPerLed + 1] = ledColors[i].R;
-        //        data[i * bytesPerLed + 2] = ledColors[i].G;
-        //        data[i * bytesPerLed + 3] = ledColors[i].B;
-        //        if (bytesPerLed == 4)
-        //            data[i * bytesPerLed + 3] = 0; // Weiß-Kanal, ggf. clever berechnen!
-        //    }
-        //    client.Send(data, data.Length, controller.IpAddress, controller.Port);
-        //}
+            var data = new byte[136 * 4 + 60 * 3 + 23 * 3 + 3];
+            data[0] = 3;
+            for (int i = 0; i < ledColors.Count; i++)
+            {
+                data[i * bytesPerLed + 1] = ledColors[i].R;
+                data[i * bytesPerLed + 2] = ledColors[i].G;
+                data[i * bytesPerLed + 3] = ledColors[i].B;
+                if (bytesPerLed == 4)
+                    data[i * bytesPerLed + 3] = 0; // Weiß-Kanal, ggf. clever berechnen!
+            }
+            client.Send(data, data.Length, controller.IpAddress, controller.Port);
+        }
     }
 }
